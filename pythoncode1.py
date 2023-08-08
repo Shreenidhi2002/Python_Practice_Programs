7ffb8cc76bd5eecf85bc26263ba477f3c88b9023
Shreenidhi2002 <shreenidhirao2@gmail.com>
2023-08-08 21:59:27 +0530

    added python file

---
+++ pythoncode1.py
@@ -0,0 +1,8 @@
+price = 1000000
+has_good_credit = True
+if has_good_credit :
+    down_payment = (10/100) * price
+else :
+    down_payment = (20/100) * price
+print(f"Downpayment :{down_payment}")
+
